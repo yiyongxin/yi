60d446fe51b2004e856def0d6e533395e18b287e
yiyongxin <568477647@qq.com>
2018-03-31 15:35:28 +0800

    log update

--- src/public/logifs.h
+++ src/public/logifs.h
@@ -18,14 +18,34 @@ extern log4cplus::Logger _logger_info;
 extern log4cplus::Logger _logger_warn;
 extern log4cplus::Logger _logger_fatal;
 
-#define LOGIFS_ERR(str)     {LOG4CPLUS_ERROR(_logger_err,str);}
+#define LOGIFS_ERR(str)\
+     {\
+        LOG4CPLUS_ERROR(_logger_err,__FILE__ << __LINE__ << __FUNCTION__);\
+        LOG4CPLUS_ERROR(_logger_err,str);\
+    }
 
-#define LOGIFS_INFO(str)    {LOG4CPLUS_INFO(_logger_info,str);}
+#define LOGIFS_INFO(str)\
+    {\
+        LOG4CPLUS_INFO(_logger_info,__FILE__ << __LINE__ << __FUNCTION__);\
+        LOG4CPLUS_INFO(_logger_info,str);\
+    }
 
-#define LOGIFS_DEBUG(str)   {LOG4CPLUS_DEBUG(_logger_debug,str);}
+#define LOGIFS_DEBUG(str)\
+    {\
+        LOG4CPLUS_DEBUG(_logger_debug,__FILE__ << __LINE__ << __FUNCTION__);\
+        LOG4CPLUS_DEBUG(_logger_debug,str);\
+    }
 
-#define LOGIFS_WARN(str)    {LOG4CPLUS_WARN(_logger_warn,str);}
+#define LOGIFS_WARN(str)\
+    {\
+        LOG4CPLUS_WARN(_logger_warn,__FILE__ << __LINE__ << __FUNCTION__);\
+        LOG4CPLUS_WARN(_logger_warn,str);\
+    }
 
-#define LOGIFS_FATAL(str)   {LOG4CPLUS_FATAL(_logger_fatal,str);}
+#define LOGIFS_FATAL(str)\
+    {\
+        LOG4CPLUS_FATAL(_logger_fatal,__FILE__ << __LINE__ << __FUNCTION__);\
+        LOG4CPLUS_FATAL(_logger_fatal,str);\
+    }
 
 #endif
