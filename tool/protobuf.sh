6a159a67d41c063b0339582f9cf06a04f1f228a6
yiyongxin <568477647@qq.com>
2019-05-15 23:52:15 +0800

    update protobuf cmake

--- tool/protobuf.sh
+++ tool/protobuf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $(find ../protocol -name *.proto); do
+for file in $(find ../protocol -name *.proto | grep -v discard); do
     echo $file
 	protoc --cpp_out=../src/protobuf  --proto_path=../protocol $file
 done
